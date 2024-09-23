588bc6e6c7d01044828008ca287d345085db51ba
Omkar-28 <omikhatavkar2@gmail.com>
2024-09-23 15:57:39 +0530

    Added Next Js Auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,18 @@
-import '@/app/ui/global.css';
-import { inter } from './ui/fonts';
+import "@/app/ui/global.css";
+import {inter} from "./ui/fonts";
+import { Metadata } from "next";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
-    </html>
-  );
+export const metadata: Metadata = {
+	title: "Acme Dashboard",
+	description:
+		"The official Next.js Course Dashboard, built with App Router.",
+	metadataBase: new URL("https://next-js-dashboard-ixggsoiy8-omkar28s-projects.vercel.app/"),
+};
+
+export default function RootLayout({children}: {children: React.ReactNode}) {
+	return (
+		<html lang="en">
+			<body className={`${inter.className} antialiased`}>{children}</body>
+		</html>
+	);
 }
